d4672458cfb402ae27522feeee6575283469d5d1
liarch29 <charliean19@gmail.com>
2018-12-14 19:30:20 -0800

    modified node

--- node.js
+++ node.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  //res.sendFile(path.join(__dirname + '/index.html'));
+  res.send("adsfasdc");
 });
 
 app.listen(process.env.PORT || 3000);
