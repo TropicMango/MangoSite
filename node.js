f4969128c5b27bfbc912b27c33ff7aa9a1cb890d
liarch29 <charliean19@gmail.com>
2018-12-14 20:31:36 -0800

    Update node.js

--- node.js
+++ node.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.get('/', function (req, res) {
   
-  res.sendFile('public/index.html', {root: path.dirname(__dirname)});
-  //res.send('did i break this?');
+  //res.sendFile('public/index.html', {root: path.dirname(__dirname)});
+  res.send('did i break this?');
 });
 
 app.listen(process.env.PORT || 3000);
