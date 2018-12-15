5f3d0092b862bf1f5865099f7e23799a43ba92b9
liarch29 <charliean19@gmail.com>
2018-12-15 02:01:18 -0800

    Update node.js

--- node.js
+++ node.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  
   res.sendFile('template/index.html', {root: __dirname});
-  //res.send('Mango site :3');
 });
+
+app.get('/nav.html', function (req, res) {
+  res.sendFile('template/nav.html', {root: __dirname});
+});
+
 
 app.listen(process.env.PORT || 3000);
