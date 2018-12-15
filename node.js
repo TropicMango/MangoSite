746e4fc6af42e1256c03baea685a63f3740afd7f
liarch29 <charliean19@gmail.com>
2018-12-14 19:21:15 -0800

    changed file name

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__basedir + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
