d0037769eb1d53c67539816b5362c7728c704524
liarch29 <charliean19@gmail.com>
2018-12-17 01:19:18 -0800

    updated project html

--- node.js
+++ node.js
@@ -5,12 +5,8 @@ app.get('/', function (req, res) {
   res.sendFile('src/template/index.html', {root: __dirname});
 });
 
-app.get('/game', function (req, res) {
-  res.sendFile('src/template/game.html', {root: __dirname});
-});
-
-app.get('/nav.css', function (req, res) {
-  res.sendFile('src/template/nav.css', {root: __dirname});
+app.get('/project', function (req, res) {
+  res.sendFile('src/template/project.html', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 3000);
