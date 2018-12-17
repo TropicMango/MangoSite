18bc639155055473d3ef07dbd9225740245dc93b
liarch29 <charliean19@gmail.com>
2018-12-16 17:38:35 -0800

    deployment test for css file

--- node.js
+++ node.js
@@ -13,8 +13,8 @@ app.get('/include.js', function (req, res) {
   res.sendFile('src/style/include.js', {root: __dirname});
 });
 
-app.get('/Mango.png', function (req, res) {
-  res.sendFile('src/img/Mango.png', {root: __dirname});
+app.get('/nav.css', function (req, res) {
+  res.sendFile('src/template/nav.css', {root: __dirname});
 });
 
 app.listen(process.env.PORT || 3000);
